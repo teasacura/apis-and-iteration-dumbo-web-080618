d3486e2bc71f21fcf2a426c7abdc8da8e758119d
teasacura <teasacuraser@gmail.com>
2018-08-08 09:41:36 -0400

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,14 +2,36 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+
+def get_api_info(url)
+  string = RestClient.get(url)
+  hash = JSON.parse(string)
+end
+
+
 def get_character_movies_from_api(character)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
+
+  response_hash = get_api_info('http://www.swapi.co/api/people/')
+  # response_string = RestClient.get('http://www.swapi.co/api/people/')
+  # response_hash = JSON.parse(response_string)
+
+  films_hash = []
+
+  # NOTE: in this demonstration we name many of the variables _hash or _array.
   # This is done for educational purposes. This is not typically done in code.
 
+
+  response_hash["results"].each do |char|
+    if char["name"].downcase == character.downcase
+      char.each do |key, value|
+        if key == "films"
+          films_hash << value
+        end
+      end
+    end
+  end
+  films_hash.flatten
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -23,13 +45,26 @@ def get_character_movies_from_api(character)
 end
 
 def print_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+
+  films_hash.each do |film|
+    movie_hash = get_api_info(film)
+    # response_string = RestClient.get(film)
+    # movie_hash = JSON.parse(response_string)
+
+    puts "=============="
+    puts movie_hash["title"]
+    puts "=============="
+
+  end
 end
+
 
 def show_character_movies(character)
   films_array = get_character_movies_from_api(character)
   print_movies(films_array)
 end
+
+
 
 ## BONUS
 
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,8 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome to Star Wars Movie Finder!"
 end
 
 def get_character_from_user
   puts "please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  input = gets.strip
 end
